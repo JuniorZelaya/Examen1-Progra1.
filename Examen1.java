440eb9ca01d1cd4315ae6d0d42bbb3ec628b2f05
JuniorZelaya <JuniorZelaya26@gmail.com>
2014-08-18 15:10:57 -0600

    Update Examen1.java

--- Examen1.java
+++ Examen1.java
@@ -3,36 +3,71 @@ public class Examen1
  	public static void main (Strings args[]) 
 			 
 			//devuelve x multiplicado por 2 (1pt)
-			 boolean esmultiplo(int n1,int n2)
+			 //creamos una variable y la multiplico por 2
+	
 			{
-				if (n1%n2==0)
-					return true;
-				else
-					return false;
-			}
+	                	return x * 2 ;
+                           }
 		
 			
 			//devuelve el numero mayor entre a, b, c y d (2pts)
 			int getMayor(int a, int b, int c, int d)
 			{
-				 int N = 1;
-			        System.out.println("Valor inicial de N = " + N);
-			        N+=22;
-			        System.out.println("N + 22 = " + N);
-			        N-=4;
-			        System.out.println("N - 4 = " + N);
-			        N*=6;
-			        System.out.println("N * 2 = " + N);
-			    }
+		if(a > b && a > c && a > d)
+		{
+			System.out.println(a + " es el numero mayor");
+			return a;
+		}
+		else if(b > a && b > c && b > d)
+		{
+			System.out.println(b + " es el numero mayor");
+			return b;
+		}
+		else if(c > a && c > b && c > d) // similar al ejercicio de tarea ya que creamos todas las variables
+		{
+			System.out.println(c + " es el numero mayor");
+			return c;
+		}
+		else if(d > a && d > b && d > c)
+		{
+			System.out.println(d + " es el numero mayor");
+			return d;
+		}
+		else
+		{
+			System.out.println("Resultado: "+ a +" (Todos los numeros son iguales!)"); // creamos al final 
+			return a;
+		}
+	}
 	
 				
 			
 			//Pide 2 nombres de personas.
 			//Si los nombres son iguales imprime "Tocayos" de lo contrario imprime "No son tocayos". (2pts)
 			void sonTocayos()
-			{
-				
-			}
+			//hay que crear el string para que nos deje poner texto
+			
+			String nom1;
+		String nom2;
+		System.out.println("ingrese Nombre: ");
+           nom1=s.next();
+           System.out.println("ingrese otro nombre: ");  //pedimos que ingrese la segunda variable
+           nom2=s.next();
+           {
+        	   if(nom1.equals(nom2 ))
+        	   {
+        		   
+           
+        		System.out.println("son tocayos ");
+        	   
+        	   }else 
+        	   {
+        		   System.out.println("no son tocayos ");
+        	   }
+           }
+	}
+	
+			
 			
 			//Imprime 100 numeros que sean multiplos de 5(3pts)
 			void imprimirMultiplosDe5()
@@ -64,13 +99,21 @@ public class Examen1
 			//Pide 4 numeros, si todos son pares devuelve true, de lo contrario devuelve false (4puntos)
 			boolean sonPares()
 			{
-				return false;
-			}
+		
+	if( edad >= 25 && promedio >= 85)
+	{
+	return true;
+	}
+	return false;
+		}
 			
 			//Pueden probar sus funciones en el main pero no es necesario ni será evaluado
 			public static void main(String args[])
 			{
 				
+				
+				
+				//Algunos ejercicios se los copie a Mike Sabillon
 			}
 		}
 	}
