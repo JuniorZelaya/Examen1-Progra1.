bf16d7a0606c265a5ad2a830501eb4714df6e852
Alumno <alumno@localhost.localdomain>
2014-08-18 08:23:52 -0600

    primer commit

---
+++ Examen1.java
@@ -0,0 +1,63 @@
+public class Examen1
+
+ 	public static void main (Strings args[]) 
+			 
+			//devuelve x multiplicado por 2 (1pt)
+			 boolean esmultiplo(int n1,int n2)
+			{
+				if (n1%n2==0)
+					return true;
+				else
+					return false;
+			}
+		
+			
+			//devuelve el numero mayor entre a, b, c y d (2pts)
+			int getMayor(int a, int b, int c, int d)
+			{
+				 int N = 1;
+			        System.out.println("Valor inicial de N = " + N);
+			        N+=22;
+			        System.out.println("N + 22 = " + N);
+			        N-=4;
+			        System.out.println("N - 4 = " + N);
+			        N*=6;
+			        System.out.println("N * 2 = " + N);
+			    }
+	
+				
+			
+			//Pide 2 nombres de personas.
+			//Si los nombres son iguales imprime "Tocayos" de lo contrario imprime "No son tocayos". (2pts)
+			void sonTocayos()
+			{
+				
+			}
+			
+			//Imprime 100 numeros que sean multiplos de 5(3pts)
+			void imprimirMultiplosDe5()
+			{
+				
+			}
+			
+			//Devuelve true si edad es menor o igual a 25 y promedio mayor o igual a 85 (3pts)
+			boolean aplicaBeca(int edad, int promedio)
+			{
+				return false;
+			}
+			
+			//Pide 4 numeros, si todos son pares devuelve true, de lo contrario devuelve false (4puntos)
+			boolean sonPares()
+			{
+				return false;
+			}
+			
+			//Pueden probar sus funciones en el main pero no es necesario ni será evaluado
+			public static void main(String args[])
+			{
+				
+			}
+		}
+	}
+}
+
